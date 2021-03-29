4757a1b279a51f1ce95e179f997085dc9211bcc8
Bruno Croci <crocidb@gmail.com>
2021-03-29 20:31:37 +0200

    adding basic Vector3 data type

--- Main.hs
+++ Main.hs
@@ -3,6 +3,24 @@ module Main where
 import System.Console.ANSI
 import Control.Monad
 
+-- 3D Math
+
+data Vector = Vector3 Float Float Float 
+    deriving (Show)
+
+vecGetX (Vector3 x _ _) = x
+vecGetY (Vector3 _ y _) = y
+vecGetZ (Vector3 _ _ z) = z
+
+vecAdd (Vector3 x y z) (Vector3 x' y' z') = Vector3 (x + x') (y + y') (z + z')
+vecSub (Vector3 x y z) (Vector3 x' y' z') = Vector3 (x - x') (y - y') (z - z')
+vecMul (Vector3 x y z) f = Vector3 (x * f) (y * f) (z * f)
+vecLength (Vector3 x y z) = sqrt (x*x + y*y + z*z)
+vecNormalize (Vector3 x y z) = Vector3 (x / length) (y / length) (z / length)
+    where length = vecLength (Vector3 x y z)
+
+-- Screen Rendering specifics
+
 renderScreen w h = do
     renderLine w h
     when (w > 0) $ renderScreen (w - 1) h
@@ -18,4 +36,4 @@ getPixelAt x y = do
 main :: IO ()
 main = do
     clearScreen
-    renderScreen 10 50
+    renderScreen 25 120
